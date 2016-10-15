4eb5279ff55da813be219259225913da3e4ab7e2
201311203__j <xodbd9756@naver.com>
2016-10-15 21:28:46 +0900

    package-1

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+package com.sd.hello;
+
+public class HelloWorld {
+	
+	
+	public void hello(){
+		System.out.println("Hello world");
+	}
+	
+	public void hello(String name){
+		System.out.printf("Hello %s", name);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,World--");
+	}
+
+}
