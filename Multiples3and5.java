424f7580bd8dc77aa8e3aaaabf1e4db12d241200
201311203__j <xodbd9756@naver.com>
2016-11-06 23:50:44 +0900

    11/06

---
+++ Multiples3and5.java
@@ -0,0 +1,19 @@
+package com.sd.Draw;
+
+public class Multiples3and5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum = 0;
+		for(int i = 1; i < 1000; i++){
+
+			if(i%3 == 0 || i%5 == 0){
+				sum += i;
+			}
+		}
+		
+		System.out.println(sum);
+	}
+
+}
